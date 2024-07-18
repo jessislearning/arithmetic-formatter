ae2ada0d03671187cf65025a167deaf8861bee53
Jessica A <161026755+jessislearning@users.noreply.github.com>
2024-07-18 21:08:19 +0800

    Update arithmetic_arranger.py
    Starting the code with checking the format of the inputted problems

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,23 @@
 def arithmetic_arranger(problems):
+    # Error 1: too many problems
+    if len(problems) > 5:
+        return "Error: Too many problems."
 
+    # Error 2: operator is not + or -
+    acceptable_char = "0123456789-+ "
+    for equation in problems:
+        for char in equation:
+            if char not in acceptable_char:
+                return "Error: Operator must be '+' or '-'."
+            
+    # Error 3: operands must only contain numbers
+    # Error 4: operands have more than 4 digits
+
+    #split the equation into A (operand) B format:
+    separated = []
+    for equation in problems:
+        separated.append(equation.split())
+    
+    #print(separated)
 
     return arranged_problems
